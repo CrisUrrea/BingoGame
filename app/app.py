34ec267dbb52d034ad2f0e486cbf8f5c8a0c34ca
CrisUrrea <cristhianca2009@gmail.com>
2023-10-02 16:07:26 -0500

    Modelo carta

--- app/app.py
+++ app/app.py
@@ -3,14 +3,18 @@ from flask_socketio import SocketIO, emit
 import random
 import time
 
+# Declaracion Flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 socketio = SocketIO(app)
 
+# Variables globales
 juego_iniciado = False
 numeros_sorteados = []
 numeros_registrados = []
 tiempo_entre_balotas = 5
+balotas = list(range(1, 76))
+markedNumbers = {}
 
 class GeneradorLinealCongruente:
     def __init__(self, semilla, a, c, m):
@@ -33,17 +37,34 @@ def generar_balota():
         return balota
     return None
 
-def generar_balota():
-    if balotas:
-        balota = random.choice(balotas)
-        balotas.remove(balota)
-        return balota
-    return None
+def generar_tabla_de_bingo():
+    bingo_table = []
+    numeros_disponibles = {  # Rangos para cada columna
+        'B': list(range(1, 16)),
+        'I': list(range(16, 31)),
+        'N': list(range(31, 46)),
+        'G': list(range(46, 61)),
+        'O': list(range(61, 76))
+    }
 
+    for _ in range(5):
+        columna = []
+        for letra in 'BINGO':
+            # Elegir un número aleatorio dentro del rango correspondiente
+            numero = random.choice(numeros_disponibles[letra])
+            numeros_disponibles[letra].remove(numero)
+            columna.append(numero)
+        bingo_table.append(columna)
+
+    return bingo_table
+
+# Rutas
+# Pestaña Index
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Pestaña Tablero
 @app.route('/tablero/', methods=['GET', 'POST'])
 def tablero():
     global juego_iniciado
@@ -72,8 +93,16 @@ def tablero():
         elif request.form['action'] == 'ordenar':
             juego_iniciado = False
             return redirect(url_for('ordenar_numeros'))
-    return render_template('tablero.html', juego_iniciado=juego_iniciado, numeros_sorteados=numeros_sorteados, tiempo_entre_balotas=tiempo_entre_balotas)
+    return render_template('tablero.html', juego_iniciado=juego_iniciado, numeros_sorteados=numeros_sorteados, tiempo_entre_balotas=tiempo_entre_balotas, markedNumbers=markedNumbers)
 
+# Pestaña Bingo
+@app.route('/bingo')
+def bingo():
+    # Llama a la función para generar la tabla de bingo
+    bingo_table = generar_tabla_de_bingo()
+    return render_template('bingo.html', bingo_table=bingo_table)
+
+# Ordenador
 @app.route('/ordenar_numeros', methods=['POST'])
 def ordenar_numeros():
     global numeros_sorteados
@@ -81,10 +110,7 @@ def ordenar_numeros():
         numeros_sorteados.sort()
     return redirect(url_for('tablero'))
 
-@app.route('/bingo')
-def bingo():
-    return render_template('bingo.html')
-
+# Conexion Socket
 @socketio.on('connect')
 def handle_connect():
     if juego_iniciado:
@@ -100,8 +126,40 @@ def sortear_balotas():
         if balota:
             numeros_sorteados.append(balota)
             numeros_registrados.append(balota)
+            markedNumbers[str(balota)] = False
             socketio.emit('update_balota', {'balota': balota})
             time.sleep(tiempo_entre_balotas)
+
+# Función para verificar el bingo
+@app.route('/verificar_bingo', methods=['POST'])
+def verificar_bingo():
+    global numeros_sorteados
+    global numeros_registrados
+
+    if juego_iniciado:
+        # Obtén los números marcados desde la solicitud POST como una cadena
+        numeros_marcados_str = request.form.get('numeros_marcados')
+        
+        # Dividir la cadena en una lista de números únicos
+        numeros_marcados_lista = list(set(numeros_marcados_str.split(',')))
+        
+        # Convierte los números en enteros
+        numeros_marcados_revision = [int(numero.strip()) for numero in numeros_marcados_lista]
+        
+        print('Números marcados recibidos:', numeros_marcados_revision)
+        print("Longitud:", len(numeros_marcados_revision))
+        print("Sorteados:", numeros_sorteados)
+        
+        if len(numeros_marcados_revision) == 25:  # Se han marcado todos los números del tablero
+            if set(numeros_marcados_revision).issubset(set(numeros_sorteados)):
+                return "Ganaste el Bingo"
+            else:
+                return "No todos los números han sido anunciados"
+        else:
+            return "Aún no has marcado todos los números del tablero"
+    else:
+        return "El juego no está en curso"
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- app/static/js/bingo.js
+++ app/static/js/bingo.js
@@ -1,37 +1,62 @@
+// Objeto para rastrear números marcados
 var markedNumbers = {};
 
+// Objeto para rastrear números marcados
+var contadorNumerosMarcados = 0;
+
+// Función para marcar o desmarcar un número
 function marcarNumero(cell) {
     var number = cell.textContent;
     if (markedNumbers[number]) {
         cell.style.backgroundColor = '';
-        delete markedNumbers[number];
+        markedNumbers[number] = false;
+        contadorNumerosMarcados--; // Resta 1 al contador
     } else {
         cell.style.backgroundColor = 'lightblue';
         markedNumbers[number] = true;
+        contadorNumerosMarcados++; // Suma 1 al contador
     }
+
+    // Actualiza el valor del campo oculto con la lista de números marcados
+    let numerosMarcados = Object.keys(markedNumbers).filter(numero => markedNumbers[numero]);
+    document.getElementById('numerosMarcados').value = numerosMarcados;
+
+    // Actualiza el texto del párrafo de contador
+    document.getElementById('contadorNumerosMarcados').textContent = contadorNumerosMarcados + ' de 25 números marcados';
+    // No emitimos el número seleccionado al servidor aquí; lo hacemos al verificar el Bingo
 }
 
-function verificarBingo() {
-    var rows = document.querySelectorAll('tr');
-    var bingo = true;
-    for (var i = 0; i < rows.length; i++) {
-        var row = rows[i];
-        var cells = row.querySelectorAll('td');
-        for (var j = 0; j < cells.length; j++) {
-            var cell = cells[j];
-            var number = cell.textContent;
-            if (markedNumbers[number] !== true) {
-                bingo = false;
-                break;
-            }
-        }
-        if (!bingo) {
-            break;
-        }
-    }
-    if (bingo) {
-        document.getElementById('bingo-message').textContent = '¡Ganaste! ¡Es un Bingo!';
+// Agregar un evento al botón "Bingo" para verificar el Bingo
+const btnVerificarBingo = document.querySelector('#btnVerificarBingo');
+btnVerificarBingo.addEventListener('click', function () {
+    // Comprobar si se han marcado todos los números (contador igual a 25)
+    if (contadorNumerosMarcados === 25) {
+        socket.emit('verificar_bingo');
     } else {
-        document.getElementById('bingo-message').textContent = 'Sigue intentando...';
+        alert('Debes marcar todos los números del tablero antes de verificar el Bingo.');
     }
-}
+});
+
+// Declarar socket como variable global
+const socket = io.connect('http://' + document.domain + ':' + location.port);
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Manejar la conexión al servidor Socket.IO
+    socket.on('connect', function () {
+        console.log('Conectado al servidor de Socket.IO');
+    });
+
+    // Obtener el botón de reinicio
+    const btnreinicio = document.querySelector("#btnNewCard");
+
+    // Función para reiniciar la página
+    const reinicioPagina = () => {
+        btnreinicio.addEventListener('click', (e) => {
+            e.preventDefault();
+            location.reload();
+        });
+    }
+
+    // Funciones
+    reinicioPagina();
+});
